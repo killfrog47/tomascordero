6cba3855a9d8581a819c30e553dd2dfabe3b8bed
Tomas Cordero <tcorderoiv@gmail.com>
2016-01-23 11:27:02 -0700

    updated home page and started about me page

--- assets/js/master.js
+++ assets/js/master.js
@@ -10,28 +10,74 @@ var backgrounds = ['home-img-01.jpg','home-img-02.jpg','home-img-03.jpg','home-i
 function change_bg(counter){
 	var new_bg = backgrounds[counter];
 	$('.background').delay('5000').fadeOut('250', function() {
-		$(this).css('background-image', 'url(assets/images/home-page/'+new_bg);
+		$(this).css('background-image', 'url(/assets/images/home-page/'+new_bg);
 	}).fadeIn('250');
+	if(counter >= backgrounds.length - 1){
+		counter = 0;
+	}else{
+		counter++;
+	}
 	setTimeout(function(){
-		if(counter >= (backgrounds.length - 1)){
-			change_bg(0);
-		}else{
-			change_bg(counter + 1);
-		}
+		change_bg(counter);
 	}, 5000);
-
 }
+
+function updateLoader(precentDone){
+	if(precentDone < 100){
+		$(".loadingBox .status").css('width', precentDone + '%');
+	}else{
+		$(".loadingBox").remove();
+	}
+}
+
+// function preload(arrayOfImages) {
+// 	var progress = 0,
+// 		numberOfItems = arrayOfImages.length,
+// 		precentDone = 0;
+
+//     $(arrayOfImages).each(function () {
+//         console.log(progress);
+//         $('<img />').attr('src','assets/images/home-page/'+this).appendTo('.background');
+//         if(progress >= 0){
+//         	precentDone = progress/numberOfItems * 100;
+//         }
+//         updateLoader(precentDone);
+//         progress++;
+//     });
+//     // $('.background img').eq(0).addClass('active');
+// }
+function preload(imageArray, index) {
+        index = index || 0;
+        if (imageArray && imageArray.length > index) {
+            var img = new Image ();
+            img.onload = function() {
+                preload(imageArray, index + 1);
+            }
+            img.src = '/assets/images/home-page/' + imageArray[index];
+        }else{
+        	$('body').addClass('page-loaded');
+        	change_bg(0);
+        }
+}
+$(window).load(function() {
+	preload(backgrounds);
+});
 
 $(function(){
 	var headerHeight = $('header').height(),
 		logoHeight = $('.intro-box').height(),
 		topCalc = (headerHeight / 2) - (logoHeight / 2);
 
+	
+
+	// $('.background img').eq(0).addClass('active');
+
 	$('.intro-box').css('top', topCalc+'px');
 
 	// $('.intro-box h1').hide().css('top', '0px').fadeIn(2000);
-
-	change_bg(0);
+	// if($('body').hasClass('page-loaded')){
+	// 	change_bg(0);
+	// }
 	
 	// setTimeout(function(){
 	// 	$('.intro-box h1').fadeOut(2000, function(){
